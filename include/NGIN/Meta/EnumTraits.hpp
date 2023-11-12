c8759635ab96b1716aa2bf78c13b7d803e3b9887
berggrenmille <berggrenmille@hotmail.se>
2023-11-12 23:21:23 +0100

    Create EnumTraits.hpp

---
+++ include/NGIN/Meta/EnumTraits.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <NGIN/Defines.hpp>
+
+#include <type_traits>
+
+
+namespace NGIN::Meta
+{
+    template<typename T>
+    struct EnumTraits
+    {
+        // Use std::is_enum_v<T> in the static assertion
+        static_assert(std::is_enum_v<T> && !std::is_enum_v<T>,
+                      "EnumTraits specialization is required for this enum type.");
+    };
+}// namespace NGIN::Meta
