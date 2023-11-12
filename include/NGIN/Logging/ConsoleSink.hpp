01dad1f0d786c1adbc961cf24b8267682651b3c0
berggrenmille <berggrenmille@hotmail.se>
2023-11-13 00:47:37 +0100

    Create ConsoleSink.hpp

---
+++ include/NGIN/Logging/ConsoleSink.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <NGIN/Defines.hpp>
+#include <NGIN/Logging/ISink.hpp>
+#include <iostream>
+
+namespace NGIN::Logging
+{
+    class ConsoleSink : public ISink
+    {
+    public:
+        ConsoleSink() = default;
+        ~ConsoleSink() override = default;
+
+        void Initialize() override;
+
+        void Shutdown() override;
+
+        void Log(Entry message) override;
+
+        void Flush() override;
+
+    private:
+        Bool isTerminal = false;
+
+        void SetColor(eLogLevel level);
+        void ResetColor();
+    };
+}// namespace NGIN::Logging
