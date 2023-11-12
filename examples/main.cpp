5434f1b242a2aed73d789fdf546fcd84c5f40c07
berggrenmille <berggrenmille@hotmail.se>
2023-11-12 23:51:31 +0100

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,8 +1,13 @@
 #include <NGIN/Entrypoint.hpp>
+#include <NGIN/Logging/Defines.hpp>
+#include <NGIN/Meta/EnumTraits.hpp>
 #include <iostream>
-
 int main(int argc, char* argv[])
 {
     std::cout << "Hello, World!" << std::endl;
+    for (auto a: NGIN::Meta::EnumTraits<NGIN::Logging::eLogLevel>::values)
+    {
+        std::cout << NGIN::Meta::EnumTraits<NGIN::Logging::eLogLevel>::ToString(a) << std::endl;
+    }
     return 0;
 }
