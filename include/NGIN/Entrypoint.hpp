75a07040e3702fdb502bac75cefcd390a3a110fb
berggrenmille <berggrenmille@hotmail.se>
2023-11-14 00:30:50 +0100

    Update Entrypoint.hpp

--- include/NGIN/Entrypoint.hpp
+++ include/NGIN/Entrypoint.hpp
@@ -1,12 +1,20 @@
 #pragma once
+#include <exception>
 #include <iostream>
 
 extern int NGINMain(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World! FROM NGIN" << std::endl;
-    return NGINMain(argc, argv);
+    try
+    {
+        std::cout << "Hello, World! FROM NGIN" << std::endl;
+        return NGINMain(argc, argv);
+    } catch (std::exception ex)
+    {
+        std::cerr << "NGIN Exception: " << ex.what() << std::endl;
+        return -1;
+    }
 }
 
 // Redefine main to avoid conflicts with SDL
