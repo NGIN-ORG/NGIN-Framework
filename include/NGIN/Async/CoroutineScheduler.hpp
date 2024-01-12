e26feb5b6d1e940058b6ff864d1a310dff400059
Maximiliam Berggren <berggrenmille@hotmail.se>
2024-01-12 18:03:24 +0100

    Create CoroutineScheduler.hpp

---
+++ include/NGIN/Async/CoroutineScheduler.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <NGIN/Common/Types/Primitive.hpp>
+
+namespace NGIN::Async
+{
+    class CoroutineScheduler
+    {
+    public:
+        void Tick()
+        {
+            void ScheduleA
+        }
+    }
+}
